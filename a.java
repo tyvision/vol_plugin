6cc3ee759b341757a16a61122f0b3d5363ef81a5
Anatoly <tyvision@users.noreply.github.com>
2017-05-01 23:01:02 +0300

    Delete a.java

--- a.java
+++
@@ -1,8 +0,0 @@
-private static String b(WeakReference<Context> packageInfo) {
-        packageInfo = packageInfo.get().getPackageManager().               getPackageInfo(packageInfo.get().getPackageName(),0);
-        if (Build.VERSION.SDK_INT >= 9) {
-            return "" + packageInfo.firstInstallTime + "-" + 
-                            Math.abs(new Random().nextLong());
-        }
-        return UUID.randomUUID().toString();
-    }
